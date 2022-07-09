aae9fa99ad1f97dc51aaca77c11ac6b38e420d62
gohelper <1047871481@qq.com>
2022-07-10 00:56:01 +0800

    update

--- WeGame/DataType.h
+++ WeGame/DataType.h
@@ -1,7 +1,43 @@
 #pragma once
+#include "Common.h"
 
 struct 坐标型 {
 	int x;
 	int y;
 	int z;
+};
+
+struct 地图数据
+{
+	char* 地图名称;
+	int 地图编号;
+	vector<int> 地图通道;
+	坐标型 起始坐标;
+	坐标型 终点坐标;
+	int 宽;
+	int 高;
+	vector<坐标型> 地图走法;
+	int 消耗疲劳;
+	int 通道数量;
+	__int64 临时变量;
+};
+
+struct 游戏地图
+{
+	坐标型 地图坐标;
+	bool 地图左边;
+	bool 地图右边;
+	bool 地图上边;
+	bool 地图下边;
+	int 地图通道;
+	int 背景颜色;
+};
+
+struct 地图节点
+{
+	int 地图F点;
+	int 地图G点;
+	int 地图H点;
+	坐标型 当前坐标;
+	坐标型 最终坐标;
 };
--- WeGame/GameMap.h
+++ WeGame/GameMap.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "DataType.h"
 
 // 游戏地图
 
+地图数据 寻路_地图数据();
 
+int 寻路_获取走法(vector<int> 参_地图通道, int 参_宽度, int 参_高度, 坐标型 参_地图起点, 坐标型 参_地图终点, vector<坐标型>& 参_真实走法);
+
+void 寻路_生成地图(int 参_宽度, int 参_高度, vector<int> 参_地图通道, vector<vector<游戏地图>>& 参_游戏地图);
+
+void 寻路_显示地图(vector<vector<游戏地图>> 参_地图数组, int 参_宽度, int 参_高度, vector<vector<游戏地图>>& 参_地图标签);
+
+void 寻路_路径算法(vector<vector<游戏地图>> 参_地图标签, 坐标型 参_地图起点, 坐标型 参_地图终点, int 参_宽度, int 参_高度, vector<坐标型>& 参_走法数组);
+
+int 寻路_整理坐标(vector<坐标型> 参_模拟走法, vector<坐标型>& 参_真实走法);
+
+bool  寻路_判断方向(int 参_通向, int 参_方向);
+
+int 寻路_计算方向(坐标型 参_当前房间, 坐标型 参_下个房间);
